1a3e7247f72381a70a08cdb9740be468ce9c42e2
tutu <tutu@163.com>
2023-12-09 15:56:03 +0800

    first commit

---
+++ src/main/java/org/example/GitTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * @Author:tutu
+ * @Date:Created in 15:52 2023/12/9
+ */
+public class GitTest {
+    public static void main(String[] args) {
+
+        System.out.println("hello,git");
+    }
+}
---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+/**
+ * @Author:tutu
+ * @Date:Created in ${TIME} ${DATE}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
