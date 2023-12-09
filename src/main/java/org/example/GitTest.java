3341af886b3d07e4a4b935d0e2b3db861ece9b62
TUTUhappy <118410050+TUTUhappy@users.noreply.github.com>
2023-12-09 16:53:47 +0800

    Update GitTest.java

--- src/main/java/org/example/GitTest.java
+++ src/main/java/org/example/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hello,git3,master");
         System.out.println("hello,git3,hot-fix");
         System.out.println("push,git3,hot-fix");
+        System.out.println("pull,git3,hot-fix");
     }
 }
